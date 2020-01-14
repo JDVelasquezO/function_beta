40c1da459d6f8099a8e0a3469394c6e2f383531b
JDVelasquezO <jdvela158@gmail.com>
2020-01-13 18:18:08 -0600

    getting the beta Function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueMathjax from "vue-mathjax";
+import vuetify from './plugins/vuetify';
+Vue.use(VueMathjax);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
